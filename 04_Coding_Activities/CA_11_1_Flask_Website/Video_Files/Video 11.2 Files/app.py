9ed8120d22da1d791aed1edef338c1997ffccca4
ferrarenCJ <ferrarecliff@gmail.com>
2026-08-06 13:17:05 -0700

    Added Flask Basic Example

--- 04_Coding_Activities/CA_11_1_Flask_Website/Video_Files/Video 11.2 Files/app.py
+++ 04_Coding_Activities/CA_11_1_Flask_Website/Video_Files/Video 11.2 Files/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -7,9 +7,12 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def first_route():
     #return "You issued a GET request"
-    return ''' <HTML>
-    <h1> You issued a GET request </h1>
-    '''
+    # return ''' <HTML>
+    # <h1> You issued a GET request </h1> 
+    # '''
+    return render_template("index.html", message = " hello Earthling")
+
+    
  
 
 if __name__ == "__main__":
