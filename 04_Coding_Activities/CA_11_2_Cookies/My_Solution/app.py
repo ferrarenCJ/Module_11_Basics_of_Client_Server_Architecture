74de6aa95b8681200ef1c6b38ab058243722e7b2
ferrarenCJ <ferrarecliff@gmail.com>
2026-08-07 10:54:30 -0700

    Added the solution for Required Coding Activity 11.2: Adding a Cookie to a Website

--- 04_Coding_Activities/CA_11_2_Cookies/My_Solution/app.py
+++ 04_Coding_Activities/CA_11_2_Cookies/My_Solution/app.py
@@ -11,18 +11,44 @@ def index():
 def addCookie():
     response = make_response("<h1>Cookie added!</h1>");
     # add code to add cookie here
+
+    response.set_cookie(
+        "myFirstCookie",
+        "Hello World - my first cookie!"
+    )
+
     return response
 
-@app.route("/displayCookieValue")
-def displayCookieValue():
-    cookieValue = None
-    # add code to get the cookie value (inside try block)
-    return "<h1>Cookie value: " + cookieValue + "</h1>"
 
-@app.route("/removeCookie")
-def removeCookie():
-    res = make_response("Cookie removed!")
-    # add code to remove cookie here (set max_age = 0)
+@app.route('/displayCookieValue')
+def displayCookieValue():
+
+    try:
+
+        cookieValue = request.cookies.get(
+            "myFirstCookie"
+        )
+
+        if cookieValue:
+
+            return (
+                "Found the cookie: "
+                + cookieValue
+            )
+
+        else:
+
+            return "Cookie not found!"
+
+    except:
+
+        return "Cookie not found!"
+    
+
+@app.route('/delete-cookie/')
+def delete_cookie():
+    res = make_response("Cookie Removed")
+    res.set_cookie('foo', 'bar', max_age=0)
     return res
 
 app.run(host='0.0.0.0', port=88)
